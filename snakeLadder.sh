3a6b12c2bd4319aae2f6f16d2a48971c3abddfb9
aniketchahare <anichahare@gmail.com>
2019-12-12 16:57:22 +0530

    Added: Condition for extra winning position

--- snakeLadder.sh
+++ snakeLadder.sh
@@ -3,9 +3,10 @@
 position=0
 end_Position=100
 
-while [ $position -ne $end_Position ]
+while [ $position -lt $end_Position ]
 do
 	diesValue=$((RANDOM%6+1))
+#	extraWinValue=$(($position+$diesValue))
 
 	option=$((RANDOM%3))
 	case $option in
@@ -19,9 +20,13 @@ do
 			position=$(($position-$diesValue))
 		;;
 	esac
-	if [[ $position -lt 0 || $position -gt $end_Position ]]
+
+	if [ $position -lt 0 ]
 	then
 		position=0
+	elif [ $position -gt $end_Position ]
+	then
+		position=$(($position-$diesValue))
 	fi
 	echo "Position --> $position"
 done
