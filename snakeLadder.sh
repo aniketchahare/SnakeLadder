03dbafb8c271f11b028b7e8d64ac3b091e111a7d
aniketchahare <anichahare@gmail.com>
2019-12-12 15:57:46 +0530

    Added: While loop until position 100

--- snakeLadder.sh
+++ snakeLadder.sh
@@ -1,20 +1,27 @@
 #!/bin/bash -x
 
 position=0
+end_Position=100
 
-diesValue=$((RANDOM%6+1))
+while [ $position -ne $end_Position ]
+do
+	diesValue=$((RANDOM%6+1))
 
-option=$((RANDOM%3))
-case $option in
-	0)
-		position=$position
-	;;
-	1)
-		position=$(($position+$diesValue))
-	;;
-	2)
-		position=$(($position-$diesValue))
-	;;
-esac
-
-echo "position--> $position"
+	option=$((RANDOM%3))
+	case $option in
+		0)
+			position=$position
+		;;
+		1)
+			position=$(($position+$diesValue))
+		;;
+		2)
+			position=$(($position-$diesValue))
+		;;
+	esac
+	if [[ $position -lt 0 || $position -gt $end_Position ]]
+	then
+		position=0
+	fi
+	echo "Position --> $position"
+done
